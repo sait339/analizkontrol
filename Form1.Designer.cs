864b3ff1b3cfb1e4bbb33a54c6a9575e6a68601f
ENVEROĞULARI <ENVEROULARI@ENVER-1>
2023-04-26 16:02:16 +0300

    GÜNCEL

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,65 +33,107 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(39, 37);
+            this.button1.Location = new System.Drawing.Point(3, 12);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(174, 69);
+            this.button1.Size = new System.Drawing.Size(137, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "GÜNLÜK RAPOR";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(272, 37);
+            this.button2.Location = new System.Drawing.Point(3, 68);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(174, 69);
+            this.button2.Size = new System.Drawing.Size(137, 50);
             this.button2.TabIndex = 1;
             this.button2.Text = "AYLIK RAPOR";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(487, 37);
+            this.button3.Location = new System.Drawing.Point(3, 124);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(174, 69);
+            this.button3.Size = new System.Drawing.Size(137, 50);
             this.button3.TabIndex = 2;
             this.button3.Text = "YILLIK RAPOR";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(137, 167);
+            this.button4.Location = new System.Drawing.Point(3, 180);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(174, 69);
+            this.button4.Size = new System.Drawing.Size(137, 50);
             this.button4.TabIndex = 3;
             this.button4.Text = "KAR ZARAR RAPOR";
             this.button4.UseVisualStyleBackColor = true;
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(391, 167);
+            this.button5.Location = new System.Drawing.Point(3, 236);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(174, 69);
+            this.button5.Size = new System.Drawing.Size(137, 50);
             this.button5.TabIndex = 4;
             this.button5.Text = "VARLIK RAPOR";
             this.button5.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(143, 375);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(149, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(604, 351);
+            this.panel2.TabIndex = 6;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(598, 292);
+            this.dataGridView1.TabIndex = 0;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(765, 375);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ENVEROĞULLARI GIDA";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -103,6 +145,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
