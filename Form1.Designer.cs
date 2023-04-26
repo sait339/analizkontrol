e0d142967caecf05ed45c928cad834ef519bc27b
ENVEROĞULARI <ENVEROULARI@ENVER-1>
2023-04-26 11:48:58 +0300

    TEST

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,55 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(170, 131);
+            this.button1.Location = new System.Drawing.Point(104, 73);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 69);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true
+            this.button1.Text = "GÜNLÜK RAPOR";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(385, 73);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(174, 69);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "AYLIK RAPOR";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(104, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 69);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "YILLIK RAPOR";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(385, 201);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(174, 69);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "KAR ZARAR RAPOR";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -55,6 +88,9 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
