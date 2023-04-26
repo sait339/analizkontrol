33a1e0e40b96762e8f577b4a943003233a55550a
ENVEROĞULARI <ENVEROULARI@ENVER-1>
2023-04-26 11:49:55 +0300

    TEST

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -32,11 +32,12 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(104, 73);
+            this.button1.Location = new System.Drawing.Point(39, 37);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(174, 69);
             this.button1.TabIndex = 0;
@@ -45,7 +46,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(385, 73);
+            this.button2.Location = new System.Drawing.Point(272, 37);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(174, 69);
             this.button2.TabIndex = 1;
@@ -54,7 +55,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(104, 201);
+            this.button3.Location = new System.Drawing.Point(487, 37);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(174, 69);
             this.button3.TabIndex = 2;
@@ -63,18 +64,28 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(385, 201);
+            this.button4.Location = new System.Drawing.Point(137, 167);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(174, 69);
             this.button4.TabIndex = 3;
             this.button4.Text = "KAR ZARAR RAPOR";
             this.button4.UseVisualStyleBackColor = true;
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(391, 167);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(174, 69);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "VARLIK RAPOR";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
@@ -91,6 +102,7 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
